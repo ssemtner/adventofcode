04a84356564be22311c022ab041acce72e103737
Scott Semtner <sjsemtner@gmail.com>
2023-12-14 11:52:01 -0800

    an actual day 10 solution

--- 2023/ocaml/bin/day10.ml
+++ 2023/ocaml/bin/day10.ml
@@ -30,7 +30,17 @@ let possible_directions = function
   | 'L' -> [ Up; Right ]
   | '7' -> [ Down; Left ]
   | 'F' -> [ Down; Right ]
-  | _ -> failwith "Invalid character"
+  | c -> failwithf "Invalid character %c" c ()
+;;
+
+let direction_list_to_char = function
+  | [ Up; Down ] -> '|'
+  | [ Left; Right ] -> '-'
+  | [ Up; Left ] -> 'J'
+  | [ Up; Right ] -> 'L'
+  | [ Down; Left ] -> '7'
+  | [ Down; Right ] -> 'F'
+  | _ -> failwith "Invalid direction list"
 ;;
 
 let reverse_direction = function
@@ -78,74 +88,102 @@ let add_direction (row, col) dir =
 
 let get_points grid =
   let start = find_start grid in
-  let first_move =
-    [ Up; Down; Left; Right ]
-    |> List.filter_map ~f:(fun dir ->
-      let row', col' = add_direction start dir in
-      try
-        possible_directions grid.(row').(col')
-        |> List.find ~f:(fun dir' ->
-          not (direction_equals (reverse_direction dir) dir'))
-      with
-      | _ -> None)
-  in
-  let last = List.hd_exn first_move in
+  grid.(fst start).(snd start)
+  <- [ Up; Down; Left; Right ]
+     |> List.filter_map ~f:(fun dir ->
+       let row', col' = add_direction start dir in
+       try
+         possible_directions grid.(row').(col')
+         |> List.find ~f:(fun dir' ->
+           direction_equals (reverse_direction dir) dir')
+         |> function
+         | Some _ -> Some dir
+         | None -> None
+       with
+       | _ -> None)
+     |> direction_list_to_char;
+  let first_moves = possible_directions grid.(fst start).(snd start) in
+  let first_move = List.hd_exn first_moves in
   let rec aux acc (row, col) last =
-    match grid.(row).(col) with
-    | 'S' -> acc
-    | c ->
+    match row, col with
+    | row, col when row = fst start && col = snd start -> acc
+    | row, col ->
+      let c = grid.(row).(col) in
       let next = next_direction last c in
       let next_pos = add_direction (row, col) next in
-      aux (next_pos :: acc) next_pos next
+      aux (((row, col), c) :: acc) next_pos next
   in
-  aux [] (add_direction start last) last
+  aux
+    [ start, grid.(fst start).(snd start) ]
+    (add_direction start first_move)
+    first_move
 ;;
 
 let part_1 lines =
   let grid = parse_grid lines in
   let points = get_points grid in
+  Printf.printf "points: %d\n" (List.length points);
   Float.round (List.length points // 2) |> Int.of_float
 ;;
 
-let count_inversions line j =
-  (Array.slice line 0 (j + 1) |> Array.count ~f:(fun x -> x))
-  +
-  match j, line with
-  | 0, line when line.(0) -> 1
-  | _ -> 0
+let expand_grid grid points =
+  let new_grid =
+    Array.make_matrix
+      ~dimx:(Array.length grid * 3)
+      ~dimy:(Array.length grid.(0) * 3)
+      '.'
+  in
+  List.iter points ~f:(fun ((i, j), c) ->
+    let i' = (i * 3) + 1 in
+    let j' = (j * 3) + 1 in
+    new_grid.(i').(j') <- '#';
+    possible_directions c
+    |> List.iter ~f:(fun dir ->
+      let i'', j'' = add_direction (i', j') dir in
+      new_grid.(i'').(j'') <- '#'));
+  new_grid
+;;
+
+let flood_fill grid =
+  (* Maybe not the best way to copy *)
+  let filled_grid =
+    Array.make_matrix
+      ~dimx:(Array.length grid)
+      ~dimy:(Array.length grid.(0))
+      '.'
+  in
+  Array.iteri grid ~f:(fun i row ->
+    Array.iteri row ~f:(fun j c -> filled_grid.(i).(j) <- c));
+  (* This feels very non functional but seems more efficiant than creating an entire new grid each time *)
+  let rec aux pos =
+    match pos with
+    | row, col when row < 0 || col < 0 -> ()
+    | row, col
+      when row >= Array.length filled_grid
+           || col >= Array.length filled_grid.(0) -> ()
+    | row, col when Char.( <> ) filled_grid.(row).(col) '.' -> ()
+    | row, col ->
+      filled_grid.(row).(col) <- 'O';
+      aux (row - 1, col);
+      aux (row + 1, col);
+      aux (row, col - 1);
+      aux (row, col + 1)
+  in
+  aux (0, 0);
+  filled_grid
 ;;
 
 (* SHOULD BE 541 *)
 let part_2 lines =
   let grid = parse_grid lines in
   let points = get_points grid in
-  let inversions_grid =
-    Array.make_matrix
-      ~dimx:(List.length lines)
-      ~dimy:(String.length (List.hd_exn lines))
-      false
-  in
-  List.iter points ~f:(fun (row, col) ->
-    match grid.(row).(col) with
-    | 'J' | 'L' | '|' -> inversions_grid.(row).(col) <- true
-    | _ -> ());
-  Array.map inversions_grid ~f:(fun row ->
-    Array.map row ~f:(fun c -> if c then "|" else ".")
-    |> Array.to_list
-    |> String.concat ~sep:"")
-  |> Array.to_list
-  |> String.concat ~sep:"\n"
-  |> Printf.printf "%s\n";
-  Array.foldi inversions_grid ~init:0 ~f:(fun i acc row ->
-    Array.foldi row ~init:acc ~f:(fun j acc' _ ->
-      match
-        List.exists points ~f:(fun (row', col') -> row' = i && col' = j)
-      with
-      | true -> acc'
-      | false ->
-        (match Int.rem (count_inversions row j) 2 with
-         | 0 -> acc'
-         | _ -> acc' + 1)))
+  expand_grid grid points
+  |> flood_fill
+  |> Array.foldi ~init:0 ~f:(fun i acc row ->
+    match i % 3 = 1 with
+    | false -> acc
+    | true ->
+      acc + Array.counti row ~f:(fun j c -> j % 3 = 1 && Char.equal c '.'))
 ;;
 
 (* print grid *)
