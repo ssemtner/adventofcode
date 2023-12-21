244f63b6f7473fe7ec7379267de2f2bae95fcdf3
Scott Semtner <sjsemtner@gmail.com>
2023-12-20 20:01:04 -0800

    overengineered day 19 done

--- 2023/ocaml/bin/day19.ml
+++ 2023/ocaml/bin/day19.ml
@@ -1,8 +1,15 @@
 open Core
 
-(* I way overwrote this day sorry *)
+(* I way overwrote this day *)
+(* there is also a lot of duplicated code I could clean up like *)
+(*   between the greaterthan and lessthan cases but I don't feel like it rn *)
 
 module Workflow = struct
+  type result =
+    | Accept
+    | Reject
+    | Workflow of string
+
   module Key = struct
     module T = struct
       type t =
@@ -33,24 +40,11 @@ module Workflow = struct
   end
 
   module Rule = struct
-    module Condition_result = struct
-      type t =
-        | Accept
-        | Reject
-        | Workflow of string
-
-      let to_string = function
-        | Accept -> "Accept"
-        | Reject -> "Reject"
-        | Workflow workflow -> "Workflow " ^ workflow
-      ;;
-    end
-
     type t =
       | Accept
       | Reject
-      | GreaterThan of Key.t * int * Condition_result.t
-      | LessThan of Key.t * int * Condition_result.t
+      | GreaterThan of Key.t * int * result
+      | LessThan of Key.t * int * result
       | Workflow of string
   end
 end
@@ -71,9 +65,9 @@ let parse_workflow line =
            let value = String.drop_prefix condition 2 |> Int.of_string in
            let result =
              match result with
-             | "A" -> Workflow.Rule.Condition_result.Accept
-             | "R" -> Workflow.Rule.Condition_result.Reject
-             | workflow -> Workflow.Rule.Condition_result.Workflow workflow
+             | "A" -> Workflow.Accept
+             | "R" -> Workflow.Reject
+             | workflow -> Workflow.Workflow workflow
            in
            (match String.get condition 1 with
             | '>' -> Workflow.Rule.GreaterThan (key, value, result)
@@ -96,17 +90,12 @@ let parse_part line =
          Map.add_exn acc ~key ~data)
 ;;
 
-type result =
-  | Accept
-  | Reject
-  | Workflow of string
-
 let rec process_part_workflow workflow part =
   match workflow with
   | [] -> failwith "empty workflow"
   | [ x ] ->
     (match x with
-     | Workflow.Rule.Accept -> Accept
+     | Workflow.Rule.Accept -> Workflow.Accept
      | Workflow.Rule.Reject -> Reject
      | Workflow.Rule.Workflow workflow -> Workflow workflow
      | _ -> failwith "invalid workflow")
@@ -118,46 +107,27 @@ let rec process_part_workflow workflow part =
        (match Map.find part key with
         | None -> failwith "invalid key"
         | Some value' ->
-          if value >= value'
-          then process_part_workflow xs part
-          else (
-            match result with
-            | Workflow.Rule.Condition_result.Accept -> Accept
-            | Workflow.Rule.Condition_result.Reject -> Reject
-            | Workflow.Rule.Condition_result.Workflow workflow ->
-              Workflow workflow))
+          if value >= value' then process_part_workflow xs part else result)
      | Workflow.Rule.LessThan (key, value, result) ->
        (match Map.find part key with
         | None -> failwith "invalid key"
         | Some value' ->
-          if value <= value'
-          then process_part_workflow xs part
-          else (
-            match result with
-            | Workflow.Rule.Condition_result.Accept -> Accept
-            | Workflow.Rule.Condition_result.Reject -> Reject
-            | Workflow.Rule.Condition_result.Workflow workflow ->
-              Workflow workflow))
+          if value <= value' then process_part_workflow xs part else result)
      | Workflow.Rule.Workflow workflow -> Workflow workflow)
 ;;
 
 let rec process_part workflows cur_workflow part =
   let workflow = Map.find_exn workflows cur_workflow in
   match process_part_workflow workflow part with
-  | Accept ->
-    printf " accepted %s\n" cur_workflow;
-    true
-  | Reject ->
-    printf " rejected %s\n" cur_workflow;
-    false
-  | Workflow workflow ->
-    printf " to workflow %s\n" workflow;
-    process_part workflows workflow part
+  | Accept -> true
+  | Reject -> false
+  | Workflow workflow -> process_part workflows workflow part
 ;;
 
-let part_value = Map.fold ~init:0 ~f:(fun ~key:_ ~data acc -> acc + data)
+let part_value part =
+  Map.fold part ~init:0 ~f:(fun ~key:_ ~data acc -> acc + data)
+;;
 
-(* TODO: get 446935 *)
 let part_1 _lines =
   let workflow_lines, part_lines =
     List.split_while _lines ~f:(fun line -> not (String.is_empty line))
@@ -173,37 +143,102 @@ let part_1 _lines =
   in
   let parts = List.map part_lines ~f:parse_part in
   List.fold parts ~init:0 ~f:(fun acc part ->
-    printf "process part with x=%d\n" (Map.find_exn part Workflow.Key.X);
     match process_part workflows "in" part with
-    | true ->
-      printf " accepted %d\n" (part_value part);
-      acc + part_value part
+    | true -> acc + part_value part
     | false -> acc)
 ;;
 
-(* printf "workflow length: %d\n" (List.length workflows);
-   printf "parts length: %d\n" (List.length parts);
-   let first_workflow = List.hd_exn _lines |> parse_workflow in
-   printf "first workflow: %s\n" (fst first_workflow);
-   List.iter (snd first_workflow) ~f:(fun w ->
-   match w with
-   | Workflow.Rule.Accept -> print_endline "Accept"
-   | Workflow.Rule.Reject -> print_endline "Reject"
-   | Workflow.Rule.GreaterThan (key, value, result) ->
-   printf
-   "GreaterThan %s %d (%s)\n"
-   (Workflow.Key.to_string key)
-   value
-   (Workflow.Rule.Condition_result.to_string result)
-   | Workflow.Rule.LessThan (key, value, result) ->
-   printf
-   "LessThan %s %d (%s)\n"
-   (Workflow.Key.to_string key)
-   value
-   (Workflow.Rule.Condition_result.to_string result)
-   | Workflow.Rule.Workflow workflow -> printf "Workflow %s\n" workflow); *)
+let parts_in_range range =
+  Map.fold range ~init:1 ~f:(fun ~key:_ ~data:(low, high) acc ->
+    acc * (high - low + 1))
+;;
 
-let part_2 _lines = 0
+let rec process_part_range_workflow workflow part_range accepted =
+  match workflow with
+  | [] -> failwith "empty workflow"
+  | [ x ] ->
+    (match x with
+     | Workflow.Rule.Accept -> accepted + parts_in_range part_range, []
+     | Workflow.Rule.Reject -> accepted, []
+     | Workflow.Rule.Workflow workflow -> accepted, [ workflow, part_range ]
+     | _ -> failwith "invalid workflow")
+  | x :: xs ->
+    (match x with
+     | Workflow.Rule.Accept -> accepted + parts_in_range part_range, []
+     | Workflow.Rule.Reject -> accepted, []
+     | Workflow.Rule.Workflow workflow -> accepted, [ workflow, part_range ]
+     | Workflow.Rule.GreaterThan (key, value, result) ->
+       let low, high = Map.find_exn part_range key in
+       (* if outside of range *)
+       if value > high || value < low
+       then process_part_range_workflow xs part_range accepted
+       else (
+         let yes_range = Map.set part_range ~key ~data:(value + 1, high) in
+         let no_range = Map.set part_range ~key ~data:(low, value) in
+         match result with
+         | Workflow.Accept ->
+           process_part_range_workflow
+             xs
+             no_range
+             (accepted + parts_in_range yes_range)
+         | Workflow.Reject -> process_part_range_workflow xs no_range accepted
+         | Workflow.Workflow workflow ->
+           let gt_accepted, gt_to_check =
+             process_part_range_workflow xs no_range accepted
+           in
+           gt_accepted, (workflow, yes_range) :: gt_to_check)
+     | Workflow.Rule.LessThan (key, value, result) ->
+       let low, high = Map.find_exn part_range key in
+       (* if outside of range *)
+       if value > high || value < low
+       then process_part_range_workflow xs part_range accepted
+       else (
+         let yes_range = Map.set part_range ~key ~data:(low, value - 1) in
+         let no_range = Map.set part_range ~key ~data:(value, high) in
+         match result with
+         | Workflow.Accept ->
+           process_part_range_workflow
+             xs
+             no_range
+             (accepted + parts_in_range yes_range)
+         | Workflow.Reject -> process_part_range_workflow xs no_range accepted
+         | Workflow.Workflow workflow ->
+           let lt_accepted, lt_to_check =
+             process_part_range_workflow xs no_range accepted
+           in
+           lt_accepted, (workflow, yes_range) :: lt_to_check))
+;;
+
+let rec process_part_range workflows cur_workflow part_range accepted =
+  let workflow = Map.find_exn workflows cur_workflow in
+  match process_part_range_workflow workflow part_range accepted with
+  | accepted, [] -> accepted
+  | accepted, todo ->
+    List.fold todo ~init:accepted ~f:(fun acc (workflow, part_range) ->
+      acc + process_part_range workflows workflow part_range 0)
+;;
+
+let initial_part_range () =
+  Map.of_alist_exn
+    (module Workflow.Key)
+    [ Workflow.Key.X, (1, 4000)
+    ; Workflow.Key.M, (1, 4000)
+    ; Workflow.Key.A, (1, 4000)
+    ; Workflow.Key.S, (1, 4000)
+    ]
+;;
+
+let part_2 lines =
+  let workflows =
+    List.take_while lines ~f:(fun line -> not (String.is_empty line))
+    |> List.fold
+         ~init:(Map.empty (module String))
+         ~f:(fun acc line ->
+           let name, workflow = parse_workflow line in
+           Map.add_exn acc ~key:name ~data:workflow)
+  in
+  process_part_range workflows "in" (initial_part_range ()) 0
+;;
 
 let () =
   Aoc.command
@@ -212,7 +247,7 @@ let () =
     ~path:"../data/day19.txt"
     ~test_path:(Some "../data/sample/day19.txt")
     ~test_1_target:(Some 19114)
-    ~test_2_target:None
+    ~test_2_target:(Some 167409079868000)
     ()
   |> Command_unix.run
 ;;
